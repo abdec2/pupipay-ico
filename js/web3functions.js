6bba78c16a7fc40757382be8b5206ce11ba2c2ae
abdec2 <abdec2@hotmail.com>
2022-09-11 19:46:55 +0300

    fixes done

--- js/web3functions.js
+++ js/web3functions.js
@@ -24,61 +24,61 @@ let selectedAccount;
 
 function init() {
 
-  console.log("Initializing example");
-  console.log("WalletConnectProvider is", WalletConnectProvider);
-  console.log("window.web3 is", window.web3, "window.ethereum is", window.ethereum);
+    console.log("Initializing example");
+    console.log("WalletConnectProvider is", WalletConnectProvider);
+    console.log("window.web3 is", window.web3, "window.ethereum is", window.ethereum);
 
-  // Tell Web3modal what providers we have available.
-  // Built-in web browser provider (only one can exist as a time)
-  // like MetaMask, Brave or Opera is added automatically by Web3modal
-  const providerOptions = {
-    walletconnect: {
-      package: WalletConnectProvider,
-      options: {
-        // Mikko's test key - don't copy as your mileage may vary
-        infuraId: "d68b2bf8b2e2457fb37c73aef267f910",
-      }
-    }
-  };
+    // Tell Web3modal what providers we have available.
+    // Built-in web browser provider (only one can exist as a time)
+    // like MetaMask, Brave or Opera is added automatically by Web3modal
+    const providerOptions = {
+        walletconnect: {
+            package: WalletConnectProvider,
+            options: {
+                // Mikko's test key - don't copy as your mileage may vary
+                infuraId: "d68b2bf8b2e2457fb37c73aef267f910",
+            }
+        }
+    };
 
-  web3Modal = new Web3Modal({
-    cacheProvider: false, // optional
-    providerOptions, // required
-    disableInjectedProvider: false, // optional. For MetaMask / Brave / Opera.
-  });
+    web3Modal = new Web3Modal({
+        cacheProvider: false, // optional
+        providerOptions, // required
+        disableInjectedProvider: false, // optional. For MetaMask / Brave / Opera.
+    });
 
-  console.log("Web3Modal instance is", web3Modal);
+    console.log("Web3Modal instance is", web3Modal);
 }
 
 async function fetchAccountData() {
 
-  const network = await provider.getNetwork();
-  const chainId = network.chainId;
+    const network = await provider.getNetwork();
+    const chainId = network.chainId;
 
-  if (chainId !== NETWORK_ID) {
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: `Contract is not deployed on the selected network please choose ${NETWORK_NAME} network`
-    })
-    await onDisconnect();
-    return;
-  }
+    if (chainId !== NETWORK_ID) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: `Contract is not deployed on the selected network please choose ${NETWORK_NAME} network`
+        })
+        await onDisconnect();
+        return;
+    }
 
-  // Get list of accounts of the connected wallet
-  let signer = provider.getSigner();
-  const accounts = await signer.getAddress();
+    // Get list of accounts of the connected wallet
+    let signer = provider.getSigner();
+    const accounts = await signer.getAddress();
 
-  // MetaMask does not give you all accounts, only the selected account
-  console.log("Got accounts", accounts);
-  selectedAccount = accounts;
+    // MetaMask does not give you all accounts, only the selected account
+    console.log("Got accounts", accounts);
+    selectedAccount = accounts;
 
 }
 
 async function refreshAccountData() {
-  document.querySelector("#btn-connect").style.display = 'none';
-  document.querySelector("#btn-disconnect").style.display = 'block';
-  await fetchAccountData(provider);
+    document.querySelector("#btn-connect").style.display = 'none';
+    document.querySelector("#btn-disconnect").style.display = 'block';
+    await fetchAccountData(provider);
 }
 
 /**
@@ -86,27 +86,27 @@ async function refreshAccountData() {
 */
 async function onConnect() {
 
-  console.log("Opening a dialog", web3Modal);
-  try {
-    const instance = await web3Modal.connect();
-    provider = new ethers.providers.Web3Provider(instance);
-    console.log(provider)
-  } catch (e) {
-    console.log("Could not get a wallet connection", e);
-    return;
-  }
+    console.log("Opening a dialog", web3Modal);
+    try {
+        const instance = await web3Modal.connect();
+        provider = new ethers.providers.Web3Provider(instance);
+        console.log(provider)
+    } catch (e) {
+        console.log("Could not get a wallet connection", e);
+        return;
+    }
 
-  // Subscribe to accounts change
-  window.ethereum.on("accountsChanged", (accounts) => {
-    fetchAccountData();
-  });
+    // Subscribe to accounts change
+    window.ethereum.on("accountsChanged", (accounts) => {
+        fetchAccountData();
+    });
 
-  // Subscribe to chainId change
-  window.ethereum.on("chainChanged", (chainId) => {
-    fetchAccountData();
-  });
+    // Subscribe to chainId change
+    window.ethereum.on("chainChanged", (chainId) => {
+        fetchAccountData();
+    });
 
-  await refreshAccountData();
+    await refreshAccountData();
 }
 
 
@@ -115,143 +115,141 @@ async function onConnect() {
 */
 async function onDisconnect() {
 
-  console.log("Killing the wallet connection", provider);
+    console.log("Killing the wallet connection", provider);
 
-  // TODO: Which providers have close method?
-  if (provider.close) {
-    await provider.close();
+    // TODO: Which providers have close method?
+    if (provider.close) {
+        await provider.close();
 
-    // If the cached provider is not cleared,
-    // WalletConnect will default to the existing session
-    // and does not allow to re-scan the QR code with a new wallet.
-    // Depending on your use case you may want or want not his behavir.
-    await web3Modal.clearCachedProvider();
-    provider = null;
-  }
+        // If the cached provider is not cleared,
+        // WalletConnect will default to the existing session
+        // and does not allow to re-scan the QR code with a new wallet.
+        // Depending on your use case you may want or want not his behavir.
+        await web3Modal.clearCachedProvider();
+        provider = null;
+    }
 
-  selectedAccount = null;
+    selectedAccount = null;
 
-  // Set the UI back to the initial state
-  document.querySelector("#btn-connect").style.display = "block";
-  document.querySelector("#btn-disconnect").style.display = "none";
+    // Set the UI back to the initial state
+    document.querySelector("#btn-connect").style.display = "block";
+    document.querySelector("#btn-disconnect").style.display = "none";
 }
 
 async function approve(price) {
-  try {
-    let signer = provider.getSigner();
+    try {
+        let signer = provider.getSigner();
 
-    const usdtContract = new ethers.Contract(USDT, USDTABI, signer);
-    let transaction = await usdtContract.approve(ICO_CONTRACT_ADDRESS, price, { from: selectedAccount });
-    await transaction.wait();
-    console.log(transaction)
+        const usdtContract = new ethers.Contract(USDT, USDTABI, signer);
+        let transaction = await usdtContract.approve(ICO_CONTRACT_ADDRESS, price, { from: selectedAccount });
+        await transaction.wait();
+        console.log(transaction)
 
-  } catch (e) {
-    stopLoading();
-  }
+    } catch (e) {
+        stopLoading();
+    }
 }
 
 async function buyTokenContract(price) {
-  try {
-    let signer = provider.getSigner();
-    let icoContract = new ethers.Contract(ICO_CONTRACT_ADDRESS, ICOABI, signer);
+    try {
+        let signer = provider.getSigner();
+        let icoContract = new ethers.Contract(ICO_CONTRACT_ADDRESS, ICOABI, signer);
 
-    let tx = await icoContract.buyTokens(selectedAccount, price.toString(), { from: selectedAccount });
-    console.log(icoContract);
-    await tx.wait();
-  } catch (e) {
-    stopLoading();
-  }
+        let tx = await icoContract.buyTokens(selectedAccount, price.toString(), { from: selectedAccount });
+        await tx.wait();
+        Swal.fire({
+            icon: 'success',
+            text: `Purchase Done`
+        })
+    } catch (e) {
+        stopLoading();
+    }
 }
 
 async function startLoading() {
-  document.querySelector("#btnBuy").setAttribute('disabled', 'disabled');
-  document.querySelector('#btnBuy').innerHTML = 'Processing...';
+    document.querySelector("#btnBuy").setAttribute('disabled', 'disabled');
+    document.querySelector('#btnBuy').innerHTML = 'Processing...';
 }
 
 async function stopLoading() {
-  document.querySelector("#btnBuy").removeAttribute('disabled');
-  document.querySelector('#btnBuy').innerHTML = 'Buy';
+    document.querySelector("#btnBuy").removeAttribute('disabled');
+    document.querySelector('#btnBuy').innerHTML = 'Buy';
 }
 
 async function buyTokens() {
-  let amount = document.querySelector('#amount').value;
-  if (amount === '') {
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: `Please enter Amount`
-    })
-    return;
-  }
-
-  const regExp = /^[0-9]\d*(\.\d+)?$/;
-  if (!amount.match(regExp)) {
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: `Please enter the correct amount`
-    })
-    return;
-  }
-
-  if (selectedAccount !== null && selectedAccount !== undefined) {
-    try {
-      let signer = provider.getSigner();
-      const price = ethers.utils.parseUnits(amount, "6");
-      startLoading();
-      await approve(price);
-      await buyTokenContract(price);
-      stopLoading();
-      Swal.fire({
-        icon: 'success',
-        text: `Purchase Done`
-      })
-
-    } catch (e) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: `Something went wrong`
-      })
+    let amount = document.querySelector('#amount').value;
+    if (amount === '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: `Please enter Amount`
+        })
+        return;
     }
-  } else {
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: `Connect your wallet`
-    })
-  }
+
+    const regExp = /^[0-9]\d*(\.\d+)?$/;
+    if (!amount.match(regExp)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: `Please enter the correct amount`
+        })
+        return;
+    }
+
+    if (selectedAccount !== null && selectedAccount !== undefined) {
+        try {
+            let signer = provider.getSigner();
+            const price = ethers.utils.parseUnits(amount, "6");
+            startLoading();
+            await approve(price);
+            await buyTokenContract(price);
+            stopLoading();
+        } catch (e) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: `Something went wrong`
+            })
+        }
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: `Connect your wallet`
+        })
+    }
 }
 
 async function addToken() {
-  const tokenAddress = TOKEN_CONTRACT;
-  const tokenSymbol = "PUPIPAY";
-  const tokenDecimals = 18;
-  const tokenImage = '';
+    const tokenAddress = TOKEN_CONTRACT;
+    const tokenSymbol = "PUPIPAY";
+    const tokenDecimals = 18;
+    const tokenImage = '';
 
-  try {
-    // wasAdded is a boolean. Like any RPC method, an error may be thrown.
-    const wasAdded = await window.ethereum.request({
-      method: 'wallet_watchAsset',
-      params: {
-        type: 'ERC20', // Initially only supports ERC20, but eventually more!
-        options: {
-          address: tokenAddress, // The address that the token is at.
-          symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
-          decimals: tokenDecimals, // The number of decimals in the token
-          image: tokenImage, // A string url of the token logo
-        },
-      },
-    });
+    try {
+        // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+        const wasAdded = await window.ethereum.request({
+            method: 'wallet_watchAsset',
+            params: {
+                type: 'ERC20', // Initially only supports ERC20, but eventually more!
+                options: {
+                    address: tokenAddress, // The address that the token is at.
+                    symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+                    decimals: tokenDecimals, // The number of decimals in the token
+                    image: tokenImage, // A string url of the token logo
+                },
+            },
+        });
 
-    if (wasAdded) {
-      console.log('Thanks for your interest!');
-    } else {
-      console.log('Your loss!');
+        if (wasAdded) {
+            console.log('Thanks for your interest!');
+        } else {
+            console.log('Your loss!');
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 
@@ -259,7 +257,7 @@ async function addToken() {
  * Main entry point.
  */
 window.addEventListener('load', async () => {
-  init();
-  document.querySelector("#btn-connect").addEventListener("click", onConnect);
-  document.querySelector("#btn-disconnect").addEventListener("click", onDisconnect);
+    init();
+    document.querySelector("#btn-connect").addEventListener("click", onConnect);
+    document.querySelector("#btn-disconnect").addEventListener("click", onDisconnect);
 });
