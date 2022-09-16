493f0e430b3ef5e6c42ddd875122ca28c3fddeef
abdec2 <abdec2@hotmail.com>
2022-09-16 16:29:21 +0300

    add payment in eth

--- contract/PupipayIco.sol
+++ contract/PupipayIco.sol
@@ -7,56 +7,17 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 contract PupipayIco is Crowdsale, AllowanceCrowdsale {
     using SafeMath for uint256;
 
-    IERC20 private usdt;
-
-    uint256 private __weiRaised;
-
     constructor(
-        uint256 rate, // 1,000,000,000,000 
+        uint256 rate, // 1516
         address payable wallet,
         IERC20 token,
-        address tokenWallet, 
-        IERC20 _USDT
+        address tokenWallet
     )
         AllowanceCrowdsale(tokenWallet)  
         Crowdsale(rate, wallet, token)
         public
-    {
-        usdt = _USDT;
-    }
+    {}
 
-    function weiRaised() public view returns (uint256) {
-        return __weiRaised;
-    }
-
-    function buyTokens(address beneficiary) public nonReentrant payable {
-        
-    } 
-
-    function buyTokens(address beneficiary, uint256 _weiAmount) public nonReentrant {
-        uint256 weiAmount = _weiAmount;
-        _preValidatePurchase(beneficiary, weiAmount);
-        require(usdt.transferFrom(msg.sender, wallet(), weiAmount), "usdt transfer failed");
-
-        // calculate token amount to be created
-        uint256 tokens = _getTokenAmount(weiAmount);
-
-        // update state
-        __weiRaised = __weiRaised.add(weiAmount);
-
-        _processPurchase(beneficiary, tokens);
-        emit TokensPurchased(_msgSender(), beneficiary, weiAmount, tokens);
-
-        _updatePurchasingState(beneficiary, weiAmount);
-
-        _forwardFunds();
-        _postValidatePurchase(beneficiary, weiAmount);
-    }
-
-    function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view {
-        require(beneficiary != address(0), "Crowdsale: beneficiary is the zero address");
-        require(weiAmount != 0, "Crowdsale: weiAmount is 0");
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-    }
+    
 
 }
